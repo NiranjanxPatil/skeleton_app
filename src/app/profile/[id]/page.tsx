e0031287f5bcdc36354c5ed09d6dff94dc0f1177
Niranjan <niranjan41210@gmail.com>
2023-10-24 10:50:54 +0530

    changes

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function UserProfile({params}:any){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr/>
+            <p className="text-4xl">Profile Page</p>
+            <span className="p-2 rouded-lg bg-orange ">
+            {params.id}
+            </span>
+            
+        </div>
+    )
+}
