fca7c479a5eccd0955818d1c61505d6555b1b68d
Niranjan <niranjan41210@gmail.com>
2023-10-25 23:06:27 +0530

    changes

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,13 +1,13 @@
 import { NextRequest } from "next/server";
-import  Jwt  from "jsonwebtoken";
-import { request } from "http";
+import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get("token")?.value || '';
-        const decodedToken: any = Jwt.verify(token, process.env.JWT_SECRET!);
+        const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!);
         return decodedToken.id;
     } catch (error: any) {
         throw new Error(error.message);
     }
+
 }
