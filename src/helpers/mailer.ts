7f48ca6a9959af7fac90b80fba0c41167d6dc683
Niranjan <niranjan41210@gmail.com>
2023-10-27 14:06:04 +0530

    changes

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -7,10 +7,7 @@ export const sendEmail = async({email, emailType, userId}
         try {
             // create a hased token
             const hash = await bcryptjs.hash(userId.toString(), 10)
-
-            await User.findByIdAndUpdate(UserId,
-                {resetPasswordToken: hasedToken,
-                resetPasswordExpires: Data})
+            
         } catch (error:any) {
             throw new Error(error.mesaaage);
         }
