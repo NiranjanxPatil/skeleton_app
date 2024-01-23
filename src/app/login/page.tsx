5426fcb9bd550a5a3e891e406f7d4e367c380769
Niranjan <niranjan41210@gmail.com>
2024-01-23 16:05:00 +0530

    changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -90,7 +90,7 @@ export default function LoginPage() {
               <button
                 type="button"
                 onClick={onLogin}
-                className="w-full text-white bg-primary-600 border border-white-500 hover:bg-primary-700 focus:ring-4 hover:text-extrabold focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-white bg-primary-600 border border-white-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
                 disabled={buttonDisabled || loading}
               >
                 Sign in
